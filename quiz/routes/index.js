8231e246c97b6223482878c23af6a80035324027
Unai Recio <unai.recio@agcpartners.co.uk>
2015-07-13 18:17:29 +0200

    feat(routes): Adds question and answers routes + views

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz-controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
